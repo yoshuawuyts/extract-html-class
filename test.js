d359cdd1897cfe4f0b9c244aedea6f219c4a27cc
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-07-05 12:35:23 +0200

    add duplicate test

--- test.js
+++ test.js
@@ -22,3 +22,14 @@ tape('returns empty array if no matches', function (assert) {
   assert.deepEqual(extract(html), expected, 'array was same')
   assert.end()
 })
+
+tape('removes duplicates', function (assert) {
+  var html = `
+    <div class="foo bar">
+      <p class="foo bar">hello planet</p>
+    </div>
+  `
+  var expected = [ 'foo', 'bar' ]
+  assert.deepEqual(extract(html), expected, 'array was same')
+  assert.end()
+})
